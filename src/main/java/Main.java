189118e6d0dc94c90b695c5329f3ef8827f35ac8
inigo <in1g0.s@yandex.ru>
2024-02-16 21:12:32 +0800

    Исправление других замечаний после первого ПР.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -77,38 +77,3 @@ class SplitCalculator {
         System.out.println(Formatter.format(costForEach));
     }
 }
-
-
-class Goods {
-    public String name;
-    public double price;
-
-    public Goods(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-}
-
-class Formatter {
-    public static String format(double price) {
-        String formattedPrice = String.format("%.2f", price);
-        int rubles = (int) price;
-        if (rubles % 100 >= 11 && rubles % 100 <= 14) {
-            return formattedPrice + " рублей";
-        }
-        int lastNumbers = rubles % 10;
-        return switch (lastNumbers) {
-            case 1 -> formattedPrice + " рубль";
-            case 2, 3, 4 -> formattedPrice + " рубля";
-            default -> formattedPrice + " рублей";
-        };
-    }
-}
